78f2c9e3dfd4c36efdc0611f874e3faa9a9051b1
Edward <148350643+edwardzeppelin@users.noreply.github.com>
2026-07-03 19:07:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,31 +19,6 @@ int main() {
     string amplify;
     char ch;
 
-    /*
-    while (getline(in, line)) {
-            istringstream iss(line);
-            string sentence;
-            char sign;
-
-            while (iss) {
-                sentence.clear();
-                iss >> noskipws;
-                while (iss.get(sign)) {
-                    sentence += sign;
-                    if (sign == '?' || sign == '!' || sign == '.') {
-                        break;
-                    }
-                }
-
-                if (sign == '?') {
-                    quest += sentence + '\n';
-                }
-                else if (sign == '!') {
-                    amplify += sentence + '\n';
-                }
-            }
-    }*/
-
     string sentence;
     while (getline(in, line)) {
         istringstream iss(line);
